968631c039931b6944237878a627bb443f52b13a
liwei <hi@liwei.me>
2010-12-29 16:42:30 +0800

    add setting files

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  def seo_title
+    _str = []
+    _str << @page_title if @page_title
+    _str << SITE_SETTINGS["site_title"]
+    _str.join(' - ')
+  end
+  
+  def logined?
+    !session[:user_id].nil?
+  end
+  
+  def js_void
+    "javascript:void(0);"
+  end
+  
+  def nbsp(n=4)
+    raw "&nbsp;"*n
+  end
 end
