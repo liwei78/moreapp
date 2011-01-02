dc8e416582e2f9764e62eaa2af17834fedae969d
liwei <hi@liwei.me>
2011-01-02 16:56:06 +0800

    what?

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,23 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
-
-55.times do
-  App.create(
-    :title    => Faker::Lorem.sentences(1).to_s,
-    :desc     => Faker::Lorem.paragraphs(2).join("<br />"),
-    :app_type => "iphone",
-    :icon_url => "/images/sample_icon.png",
-    :youtube_url => ( (rand(9) % 2 == 0) ? "" : "http://www.youtube.com/" )
-  )
-end
-75.times do
-  App.create(
-    :title    => Faker::Lorem.sentences(1).to_s,
-    :desc     => Faker::Lorem.paragraphs(2).join("<br />"),
-    :app_type => "ipad",
-    :icon_url => "/images/sample_icon.png",
-    :youtube_url => ( (rand(9) % 2 == 0) ? "" : "http://www.youtube.com/" )
-  )
+if Rails.env == "development"
+  55.times do
+    App.create(
+      :title    => Faker::Lorem.sentences(1).to_s,
+      :desc     => Faker::Lorem.paragraphs(2).join("<br />"),
+      :app_type => "iphone",
+      :icon_url => "/images/sample_icon.png",
+      :youtube_url => ( (rand(9) % 2 == 0) ? "" : "http://www.youtube.com/" )
+    )
+  end
+  75.times do
+    App.create(
+      :title    => Faker::Lorem.sentences(1).to_s,
+      :desc     => Faker::Lorem.paragraphs(2).join("<br />"),
+      :app_type => "ipad",
+      :icon_url => "/images/sample_icon.png",
+      :youtube_url => ( (rand(9) % 2 == 0) ? "" : "http://www.youtube.com/" )
+    )
+  end
 end
